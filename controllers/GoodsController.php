e7f789e531e1e57056786bba00a2e6ac585d9d9f
hahastein <146119@qq.com>
2018-09-13 13:00:22 +0800

    调整

---
+++ controllers/GoodsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by BengBengFramework.
+ * User: hahastein
+ * Date: 2018-09-13
+ * Time: 12:59
+ */
+
+namespace bengbeng\admin\controllers;
+
+
+use bengbeng\framework\controllers\base\FactoryController;
+
+class GoodsController extends FactoryController
+{
+//    public function
+}
