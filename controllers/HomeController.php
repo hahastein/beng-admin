927eac861b362e37e381bfeee1395fe9080c5439
hahastein <146119@qq.com>
2018-10-13 21:49:41 +0800

    update beng-admin

---
+++ controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hahastein
+ * Date: 2018/10/13
+ * Time: 21:47
+ */
+
+namespace bengbeng\admin\controllers;
+
+use bengbeng\admin\base\BaseController;
+use bengbeng\framework\base\Enum;
+
+class HomeController extends BaseController
+{
+
+    public function actionMain(){
+        \Yii::$app->Beng->outHtml('main');
+    }
+
+    public function behaviors()
+    {
+        self::setActions([
+            'main'
+        ], Enum::ACCESS_RULE_NULL);
+        return parent::behaviors();
+    }
+}
