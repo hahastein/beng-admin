5cc75fda42f50b71e5c64ca82cb37faf4956d8cf
hahastein <146119@qq.com>
2018-10-17 10:30:16 +0800

    update beng-admin

---
+++ components/assets/enum/AssetEnum.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hahastein
+ * Date: 2018/10/17
+ * Time: 0:20
+ */
+
+namespace bengbeng\admin\components\assets\enum;
+
+
+class AssetEnum
+{
+    const PLUS_SWEET_ALERT = 'sweetalert';
+    const PLUS_ICHECK = 'icheck';
+
+    static public function changePlus($plus){
+        switch ($plus){
+            case self::PLUS_SWEET_ALERT:
+                return [
+                    ''
+                ];
+        }
+    }
+
+}
