652b24c90391f711872a3bb7770fce7edfd59e16
hahastein <146119@qq.com>
2018-11-10 14:26:17 +0800

    更新功能

--- views/default/setting/sys-base.php
+++ views/default/setting/sys-base.php
@@ -1,1 +1,16 @@
-sys-base
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>Product edit</h2>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="index.html">Home</a>
+            </li>
+            <li class="breadcrumb-item">
+                <a>E-commerce</a>
+            </li>
+            <li class="breadcrumb-item active">
+                <strong>Product edit</strong>
+            </li>
+        </ol>
+    </div>
+</div>
