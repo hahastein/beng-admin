00c023e2c68c0b16b95dcdd69c3db268963b55ae
hahastein <146119@qq.com>
2018-11-15 00:22:09 +0800

    更新功能

--- views/beng/widgets/menu-left.php
+++ views/beng/widgets/menu-left.php
@@ -8,38 +8,38 @@ use yii\helpers\Url;
         <li class="m-t-30"></li>
 
         <?php foreach ($menus as $menu){ ?>
+            <?php if(!empty($menu['parent'])) { ?>
+            <li <?= $controllerID==$parent['controller']?'class="open active"':'' ?>>
+                <a href="javascript:;">
+                    <span class="title"><?=$menu['menu_name']?></span>
+                    <span class="arrow<?= $controllerID==$parent['controller']?'class=" open active"':'' ?>"></span>
+                </a>
+                <span class="icon-thumbnail">
+                    <?= empty($menu['menu_icon'])?$menu['initials']:"<i class=\"{$menu['menu_icon']}\"></i>" ?>
+                </span>
+                <ul class="sub-menu">
+                    <?php foreach ($menu['parent'] as $parent){ ?>
+                        <li <?= $controllerID==$parent['controller'] && $actionID==$parent['action']?'class="active"':'' ?>>
+                            <a href="<?= Url::to(['/'.$moduleID.'/'.$parent['controller'].'/'.$parent['action']]) ?>"><?=$parent['menu_name']?></a>
+                            <span class="icon-thumbnail">
+                                <?= empty($parent['menu_icon'])?$parent['initials']:"<i class=\"{$parent['menu_icon']}\"></i>" ?>
+                            </span>
+                        </li>
 
-            <li>
-                <?php if(!empty($menu['parent'])) { ?>
-                    <a href="javascript:;">
-                        <span class="title"><?=$menu['menu_name']?></span>
-                        <span class="arrow"></span>
-                    </a>
-                    <span class="icon-thumbnail">
-                        <?= empty($menu['menu_icon'])?$menu['initials']:"<i class=\"{$menu['menu_icon']}\"></i>" ?>
-                    </span>
-                    <ul class="sub-menu">
-                        <?php foreach ($menu['parent'] as $parent){ ?>
-                            <li <?= $controllerID==$parent['controller'] && $actionID==$parent['action']?'class="active"':'' ?>>
-                                <a href="<?= Url::to(['/'.$moduleID.'/'.$parent['controller'].'/'.$parent['action']]) ?>"><?=$parent['menu_name']?></a>
-                                <span class="icon-thumbnail">
-                                    <?= empty($parent['menu_icon'])?$parent['initials']:"<i class=\"{$parent['menu_icon']}\"></i>" ?>
-                                </span>
-                            </li>
-
-                        <?php } ?>
-                    </ul>
-                <?php }else{ ?>
-                    <a class="detailed" href="<?=Url::to(['/'.$moduleID.'/'.$menu['controller'].'/'.$menu['action']])?>">
-                        <span class="title"><?=$menu['menu_name']?></span>
-                        <span class="details">8 新消息</span>
-                    </a>
-                    <span class="icon-thumbnail">
-                        <?= empty($menu['menu_icon'])?$menu['initials']:"<i class=\"{$menu['menu_icon']}\"></i>" ?>
-                    </span>
-                <?php } ?>
+                    <?php } ?>
+                </ul>
             </li>
-
+            <?php }else{ ?>
+            <li>
+                <a class="detailed" href="<?=Url::to(['/'.$moduleID.'/'.$menu['controller'].'/'.$menu['action']])?>">
+                    <span class="title"><?=$menu['menu_name']?></span>
+                    <span class="details">8 新消息</span>
+                </a>
+                <span class="icon-thumbnail">
+                    <?= empty($menu['menu_icon'])?$menu['initials']:"<i class=\"{$menu['menu_icon']}\"></i>" ?>
+                </span>
+            </li>
+            <?php } ?>
         <?php } ?>
 
         <li class="">
