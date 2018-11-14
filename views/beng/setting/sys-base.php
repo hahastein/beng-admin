49c92740fec25284b1c3fba12c29229392627a86
hahastein <146119@qq.com>
2018-11-14 14:19:16 +0800

    更新功能

---
+++ views/beng/setting/sys-base.php
@@ -0,0 +1,13 @@
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>基本设置</h2>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="index.html">后台首页</a>
+            </li>
+            <li class="breadcrumb-item active">
+                <strong>基本设置</strong>
+            </li>
+        </ol>
+    </div>
+</div>
