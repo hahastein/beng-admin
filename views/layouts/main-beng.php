dd90d9f28538bae2ceb772aa61e5dad8dfe8a89b
hahastein <146119@qq.com>
2018-11-15 02:40:25 +0800

    更新功能

--- views/layouts/main-beng.php
+++ views/layouts/main-beng.php
@@ -9,8 +9,8 @@ TemplateAsset::register($this);
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html;charset=<?= Yii::$app->language ?>" />
-    <meta charset="<?= Yii::$app->language ?>" />
+    <meta http-equiv="content-type" content="text/html;charset=<?= Yii::$app->charset ?>" />
+    <meta charset="<?= Yii::$app->charset ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-touch-fullscreen" content="yes">
