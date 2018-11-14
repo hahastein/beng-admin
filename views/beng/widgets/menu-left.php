0e70aa04d7ca7222d253ce082bb922631604ab08
hahastein <146119@qq.com>
2018-11-15 01:04:41 +0800

    更新功能

--- views/beng/widgets/menu-left.php
+++ views/beng/widgets/menu-left.php
@@ -7,15 +7,14 @@ use yii\helpers\Url;
     <ul class="menu-items">
         <li class="m-t-30"></li>
 
-        <?php foreach ($menus as $key => $menu){ ?>
-            <?php print_r($key) ?>
+        <?php foreach ($menus as $menu){ ?>
             <?php if(!empty($menu['parent'])) { ?>
             <li <?= in_array($controllerID, $menu['child'])?'class="open active"':'' ?>>
                 <a href="javascript:;">
                     <span class="title"><?=$menu['menu_name']?></span>
                     <span class="arrow<?= in_array($controllerID, $menu['child'])?' open active':'' ?>"></span>
                 </a>
-                <span class="<?=$key===0?'bg-success ':''?>icon-thumbnail">
+                <span class="<?=$menu['is_home']?'bg-success ':''?>icon-thumbnail">
                     <?= empty($menu['menu_icon'])?$menu['initials']:"<i class=\"{$menu['menu_icon']}\"></i>" ?>
                 </span>
                 <ul class="sub-menu">
@@ -36,7 +35,7 @@ use yii\helpers\Url;
                     <span class="title"><?=$menu['menu_name']?></span>
                     <span class="details">8 新消息</span>
                 </a>
-                <span class="<?=$key===0?'bg-success ':''?>icon-thumbnail">
+                <span class="<?=$menu['is_home']?'bg-success ':''?>icon-thumbnail">
                     <?= empty($menu['menu_icon'])?$menu['initials']:"<i class=\"{$menu['menu_icon']}\"></i>" ?>
                 </span>
             </li>
