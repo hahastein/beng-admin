a1ce8c6ef3afba5f03f41a4df2183af29ca01e31
hahastein <146119@qq.com>
2018-11-15 23:07:54 +0800

    更新功能

--- views/beng/setting/sys-base.php
+++ views/beng/setting/sys-base.php
@@ -1,16 +1,25 @@
-<div class="page-content-wrapper content-builder active full-height" id="plainContent">
+<div class="page-content-wrapper content-builder active full-height" id="titleParallax">
     <!-- START PAGE CONTENT -->
     <div class="content">
+        <!-- START JUMBOTRON -->
+        <div class="jumbotron no-margin" data-pages="parallax">
+            <div class="container-fluid container-fixed-lg sm-p-l-20 sm-p-r-20">
+                <div class="inner">
+                    <h3 class="">Page Title</h3>
+                </div>
+            </div>
+        </div><!-- END JUMBOTRON -->
         <!-- START PAGE COVER -->
-        <div class=" container-fluid   container-fixed-lg">
-            <ul class="breadcrumb p-l-0">
-                <li class="breadcrumb-item"><a href="#">Home</a>
+        <div class=" container-fluid   container-fixed-lg demo-container">
+            <ul class="breadcrumb">
+                <li>
+                    <p>home</p>
                 </li>
-                <li class="breadcrumb-item active">Plain template
+                <li>
+                    <a class="active" href="#">Parallax for page title</a>
                 </li>
             </ul>
             <!-- END BREADCRUMB -->
-            <h3 class="page-title">Page Title</h3>
         </div>
         <div class=" container-fluid   container-fixed-lg">
             <!-- YOU CAN REMOVE .container-fixed-lg TO DISABLE THE FEATURE WHERE THE CONTENT
