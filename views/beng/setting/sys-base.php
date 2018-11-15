ad76cc13b03c730cfad40bb6f06743902fc226a5
hahastein <146119@qq.com>
2018-11-15 23:01:23 +0800

    更新功能

--- views/beng/setting/sys-base.php
+++ views/beng/setting/sys-base.php
@@ -1,13 +1,22 @@
-<div class="row wrapper border-bottom white-bg page-heading">
-    <div class="col-lg-10">
-        <h2>基本设置</h2>
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item">
-                <a href="index.html">后台首页</a>
-            </li>
-            <li class="breadcrumb-item active">
-                <strong>基本设置</strong>
-            </li>
-        </ol>
+<div class="page-content-wrapper content-builder active full-height" id="plainContent">
+    <!-- START PAGE CONTENT -->
+    <div class="content">
+        <!-- START PAGE COVER -->
+        <div class=" container-fluid   container-fixed-lg">
+            <ul class="breadcrumb p-l-0">
+                <li class="breadcrumb-item"><a href="#">Home</a>
+                </li>
+                <li class="breadcrumb-item active">Plain template
+                </li>
+            </ul>
+            <!-- END BREADCRUMB -->
+            <h3 class="page-title">Page Title</h3>
+        </div>
+        <div class=" container-fluid   container-fixed-lg">
+            <!-- YOU CAN REMOVE .container-fixed-lg TO DISABLE THE FEATURE WHERE THE CONTENT
+                 ALIGNS TO THE CENTER WITHOUT STRECTHING UNGLY ON 1800PX(1080P & 4/5K MONITORS) UP WORDS SCREEN
+                  -->
+            <!-- CONTENT GOES HERE-->
+        </div>
     </div>
 </div>
--- views/default/setting/sys-base.php
+++ views/default/setting/sys-base.php
@@ -1,22 +1,2 @@
-<div class="page-content-wrapper content-builder active full-height" id="plainContent">
-    <!-- START PAGE CONTENT -->
-    <div class="content">
-        <!-- START PAGE COVER -->
-        <div class=" container-fluid   container-fixed-lg">
-            <ul class="breadcrumb p-l-0">
-                <li class="breadcrumb-item"><a href="#">Home</a>
-                </li>
-                <li class="breadcrumb-item active">Plain template
-                </li>
-            </ul>
-            <!-- END BREADCRUMB -->
-            <h3 class="page-title">Page Title</h3>
-        </div>
-        <div class=" container-fluid   container-fixed-lg">
-            <!-- YOU CAN REMOVE .container-fixed-lg TO DISABLE THE FEATURE WHERE THE CONTENT
-                 ALIGNS TO THE CENTER WITHOUT STRECTHING UNGLY ON 1800PX(1080P & 4/5K MONITORS) UP WORDS SCREEN
-                  -->
-            <!-- CONTENT GOES HERE-->
-        </div>
-    </div>
-</div>
+dsfsdf
+dsfsdf
