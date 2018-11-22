a5ffc0a0906ec3d99e7c9fa7cdd61e3a1606b9c6
hahastein <146119@qq.com>
2018-11-23 02:03:36 +0800

    更新功能

--- views/beng/setting/sys-base.php
+++ views/beng/setting/sys-base.php
@@ -1,24 +1,24 @@
 <div class="page-content-wrapper">
     <!-- START PAGE CONTENT -->
-    <div class="content sm-gutter">
+    <div class="content">
 
-        <div class="container-fluid p-l-25 p-r-25 sm-p-l-0 sm-p-r-0">
-            <div class="inner">
-                <div class="">
-                    <h1 class="page-title">Page Title</h1>
-                </div>
-                <ol class="breadcrumb p-l-0">
-                    <li class="breadcrumb-item">
-                        <a href="<?=\yii\helpers\Url::home()?>">HOME</a>
-                    </li>
-                    <li class="breadcrumb-item active">
-                        <a >基本设置</a>
-                    </li>
-                </ol>
+        <div class="container-fluid container-fixed-lg">
+
+            <div class="">
+                <h1 class="page-title">Page Title</h1>
             </div>
+            <ol class="breadcrumb p-l-0">
+                <li class="breadcrumb-item">
+                    <a href="<?=\yii\helpers\Url::home()?>">HOME</a>
+                </li>
+                <li class="breadcrumb-item active">
+                    <a >基本设置</a>
+                </li>
+            </ol>
+
         </div>
 
-        <div class="container-fluid p-l-25 p-r-25 p-t-0 p-b-25 sm-padding-10">
+        <div class="container-fluid container-fixed-lg">
         </div>
 
     </div>
