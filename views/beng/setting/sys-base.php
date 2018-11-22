c6891d4946e3af1da608a9ef27c75ba7fc2f8c66
hahastein <146119@qq.com>
2018-11-23 02:20:07 +0800

    更新功能

--- views/beng/setting/sys-base.php
+++ views/beng/setting/sys-base.php
@@ -6,14 +6,14 @@
 
             <h3 class="page-title">Page Title</h3>
 
-            <ol class="breadcrumb p-l-0">
+            <ul class="breadcrumb p-l-0">
                 <li class="breadcrumb-item">
                     <a href="<?=\yii\helpers\Url::home()?>">HOME</a>
                 </li>
                 <li class="breadcrumb-item active">
                     <a >基本设置</a>
                 </li>
-            </ol>
+            </ul>
 
         </div>
 
