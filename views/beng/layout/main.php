421297e1ec9dc9c14be3c0a0b375e739d853ead4
hahastein <146119@qq.com>
2018-11-26 03:03:43 +0800

    更新功能

--- views/beng/layout/main.php
+++ views/beng/layout/main.php
@@ -84,7 +84,7 @@ use bengbeng\admin\components\handles\TemplateHandle;
                                     </li>
                                     <li>
                                         <div class="media">
-                                            <img class="img-radius" src="../files/assets/images/avatar-4.jpg" alt="Generic placeholder image">
+                                            <img class="img-radius" src="<?= TemplateHandle::getImgToTheme('images/avatar100.png')?>" alt="Generic placeholder image">
                                             <div class="media-body">
                                                 <h5 class="notification-user">John Doe</h5>
                                                 <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
@@ -94,7 +94,7 @@ use bengbeng\admin\components\handles\TemplateHandle;
                                     </li>
                                     <li>
                                         <div class="media">
-                                            <img class="img-radius" src="../files/assets/images/avatar-3.jpg" alt="Generic placeholder image">
+                                            <img class="img-radius" src="<?= TemplateHandle::getImgToTheme('images/avatar100.png')?>" alt="Generic placeholder image">
                                             <div class="media-body">
                                                 <h5 class="notification-user">Joseph William</h5>
                                                 <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
@@ -104,7 +104,7 @@ use bengbeng\admin\components\handles\TemplateHandle;
                                     </li>
                                     <li>
                                         <div class="media">
-                                            <img class="img-radius" src="../files/assets/images/avatar-4.jpg" alt="Generic placeholder image">
+                                            <img class="img-radius" src="<?= TemplateHandle::getImgToTheme('images/avatar100.png')?>" alt="Generic placeholder image">
                                             <div class="media-body">
                                                 <h5 class="notification-user">Sara Soudein</h5>
                                                 <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
