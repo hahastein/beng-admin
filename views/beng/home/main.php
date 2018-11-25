caa0636453dd9332496502702739d848d14a9ca2
hahastein <146119@qq.com>
2018-11-25 19:15:23 +0800

    更新功能

--- views/beng/home/main.php
+++ views/beng/home/main.php
@@ -1,118 +1,27 @@
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
-nbnm
-<br>
+<div class="pcoded-content">
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="page-header-title">
+                        <h4 class="m-b-10">Dashboard</h4>
+                    </div>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="index.html">
+                                <i class="feather icon-home"></i>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#!">Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pcoded-inner-content">
+asdasd
+    </div>
+</div>
