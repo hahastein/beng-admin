3d2d1a9a1f5e668fc1b2793f166731a067268a42
hahastein <146119@qq.com>
2018-11-26 02:24:07 +0800

    更新功能

--- views/beng/setting/sys-base.php
+++ views/beng/setting/sys-base.php
@@ -1,24 +1,33 @@
-<div id="plainContent" class="page-content-wrapper content-builder active full-height">
-    <!-- START PAGE CONTENT -->
-    <div class="content">
-
-        <div class="container-fluid">
-
-            <h3 class="page-title">Page Title</h3>
-
-            <ul class="breadcrumb p-l-0">
-                <li class="breadcrumb-item">
-                    <a href="<?=\yii\helpers\Url::home()?>">HOME</a>
-                </li>
-                <li class="breadcrumb-item active">
-                    <a >基本设置</a>
-                </li>
-            </ul>
-
+<div class="pcoded-content">
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="page-header-title">
+                        <h4 class="m-b-10">首页</h4>
+                    </div>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="index.html">
+                                <i class="feather icon-home"></i>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#!">基础设置</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <div class="container-fluid">
+    <div class="pcoded-inner-content">
+        <div class="main-body">
+            <div class="page-wrapper">
+                <div class="page-body">
+
+                </div>
+            </div>
         </div>
-
     </div>
 </div>
