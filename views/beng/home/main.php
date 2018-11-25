f49517486a2935fdd7ca8007ddb81aa629270cac
hahastein <146119@qq.com>
2018-11-25 19:37:20 +0800

    更新功能

--- views/beng/home/main.php
+++ views/beng/home/main.php
@@ -26,132 +26,44 @@
             <div class="page-wrapper">
                 <div class="page-body">
                     <div class="row">
-                        <div class="col-lg-5 col-md-12">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="card">
-                                        <div class="card-block">
-                                            <div class="row align-items-center">
-                                                <div class="col-8">
-                                                    <h4 class="text-c-yellow">$30200</h4>
-                                                    <h6 class="text-muted m-b-0">All Earnings</h6>
-                                                </div>
-                                                <div class="col-4 text-right">
-                                                    <i class="feather icon-bar-chart-2 f-28"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer bg-c-yellow">
-                                            <div class="row align-items-center">
-                                                <div class="col-9">
-                                                    <p class="text-white m-b-0">% change</p>
-                                                </div>
-                                                <div class="col-3 text-right">
-                                                    <i class="feather icon-trending-up text-white f-16"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="card">
-                                        <div class="card-block">
-                                            <div class="row align-items-center">
-                                                <div class="col-8">
-                                                    <h4 class="text-c-green">290+</h4>
-                                                    <h6 class="text-muted m-b-0">Page Views</h6>
-                                                </div>
-                                                <div class="col-4 text-right">
-                                                    <i class="feather icon-file-text f-28"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer bg-c-green">
-                                            <div class="row align-items-center">
-                                                <div class="col-9">
-                                                    <p class="text-white m-b-0">% change</p>
-                                                </div>
-                                                <div class="col-3 text-right">
-                                                    <i class="feather icon-trending-up text-white f-16"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="card">
-                                        <div class="card-block">
-                                            <div class="row align-items-center">
-                                                <div class="col-8">
-                                                    <h4 class="text-c-red">145</h4>
-                                                    <h6 class="text-muted m-b-0">Task</h6>
-                                                </div>
-                                                <div class="col-4 text-right">
-                                                    <i class="feather icon-calendar f-28"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer bg-c-red">
-                                            <div class="row align-items-center">
-                                                <div class="col-9">
-                                                    <p class="text-white m-b-0">% change</p>
-                                                </div>
-                                                <div class="col-3 text-right">
-                                                    <i class="feather icon-trending-down text-white f-16"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="card">
-                                        <div class="card-block">
-                                            <div class="row align-items-center">
-                                                <div class="col-8">
-                                                    <h4 class="text-c-blue">500</h4>
-                                                    <h6 class="text-muted m-b-0">Downloads</h6>
-                                                </div>
-                                                <div class="col-4 text-right">
-                                                    <i class="feather icon-thumbs-down f-28"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer bg-c-blue">
-                                            <div class="row align-items-center">
-                                                <div class="col-9">
-                                                    <p class="text-white m-b-0">% change</p>
-                                                </div>
-                                                <div class="col-3 text-right">
-                                                    <i class="feather icon-trending-down text-white f-16"></i>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <div class="card social-card bg-twitter">
-                                        <div class="card-block">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <i class="feather icon-twitter f-34 text-twitter social-icon"></i>
-                                                </div>
-                                                <div class="col">
-                                                    <h6 class="m-b-0">Twitter</h6>
-                                                    <p>231.2w downloads</p>
-                                                    <p class="m-b-0">Lorem Ipsum is simply dummy text of the printing</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <a href="#!" class="download-icon"><i class="feather icon-arrow-down"></i></a>
-                                    </div>
+                        <!-- seo analytics start -->
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card">
+                                <div class="card-block">
+                                    <h3>20500</h3>
+                                    <p class="text-muted">Total Order</p>
+                                    <div id="seo-anlytics1" style="height:50px"></div>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card">
+                                <div class="card-block">
+                                    <h3>20500</h3>
+                                    <p class="text-muted">Total Sales</p>
+                                    <div id="seo-anlytics2" style="height:50px"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card">
+                                <div class="card-block">
+                                    <h3>28000</h3>
+                                    <p class="text-muted">Order Return</p>
+                                    <div id="seo-anlytics3" style="height:50px"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card">
+                                <div class="card-block">
+                                    <h3>59600</h3>
+                                    <p class="text-muted">Total Stock</p>
+                                    <div id="seo-anlytics4" style="height:50px"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- seo analytics end -->
                     </div>
                 </div>
             </div>
