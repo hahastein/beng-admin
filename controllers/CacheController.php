d01c0ecd0738ae15518dfa5d2b7af6e09a972393
hahastein <146119@qq.com>
2018-11-26 23:34:14 +0800

    更新功能

--- controllers/CacheController.php
+++ controllers/CacheController.php
@@ -9,14 +9,36 @@
 namespace bengbeng\admin\controllers;
 
 use bengbeng\admin\base\BaseController;
+use bengbeng\admin\models\setting\ARSettingPage;
+use yii\db\ActiveQuery;
 
 class CacheController extends BaseController
 {
 
     public function actionAll(){
 
+        $settingPage = new ARSettingPage();
+        $pageData = $settingPage->dataSet(function (ActiveQuery $query){
+            $query->orderBy([
+                'page_id' => SORT_DESC
+            ]);
+        });
+
         $cache = \Yii::$app->cache;
-        \Yii::$app->Beng->outHtml($cache);
+        foreach ($pageData as $key => $item) {
+            $cache_name = 'template_'.md5($item['router']);
+
+            if($cache->exists($cache_name)){
+                $pageData[$key]['isCacheStr'] = '存在';
+                $pageData[$key]['isCache'] = true;
+            }else{
+                $pageData[$key]['isCache'] = false;
+                $pageData[$key]['isCacheStr'] = '不存在';
+            }
+
+        }
+        
+        \Yii::$app->Beng->outHtml($pageData);
 
         return $this->render('all');
     }
