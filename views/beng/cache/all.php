8c6c4aa411bc9d68f85c6cfaed75a925231bdcbd
hahastein <146119@qq.com>
2018-11-26 23:00:10 +0800

    更新功能

---
+++ views/beng/cache/all.php
@@ -0,0 +1,40 @@
+<div class="pcoded-content">
+    <div class="page-header">
+        <div class="page-block">
+            <div class="row align-items-center">
+                <div class="col-md-8">
+                    <div class="page-header-title">
+                        <h4 class="m-b-10">Dashboard</h4>
+                    </div>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="index.html">
+                                <i class="feather icon-home"></i>
+                            </a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="#!">Dashboard</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="pcoded-inner-content">
+        <div class="main-body">
+            <div class="page-wrapper">
+                <div class="page-body">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5>Default Styling</h5>
+                            <span>use class <code>table table-styling</code> inside table element</span>
+                        </div>
+                        <div class="card-block table-border-style">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
