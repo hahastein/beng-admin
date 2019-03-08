ebed8b9d04e8abdc72f06a64e01ecaf1ce28f796
hahastein <146119@qq.com>
2019-03-08 14:47:32 +0800

    更新模板功能1

--- views/beng/layout/main.php
+++ views/beng/layout/main.php
@@ -7,7 +7,7 @@ use yii\helpers\Html;
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="<?= Yii::$app->language ?>">
+    <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
