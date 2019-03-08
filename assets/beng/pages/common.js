706df183a8efc03f055d922998cff5b2f17e5163
hahastein <146119@qq.com>
2019-03-08 20:21:04 +0800

    更新模板功能1

--- assets/beng/pages/common.js
+++ assets/beng/pages/common.js
@@ -4,5 +4,10 @@ $(function () {
         $(".preloader").fadeOut();
     });
 
+    $(".right-side-toggle").click(function () {
+        $(".right-sidebar").slideDown(50);
+        $(".right-sidebar").toggleClass("shw-rside");
+    });
+
     $('.scroll-sidebar, .right-side-panel, .message-center, .right-sidebar').perfectScrollbar();
 });
