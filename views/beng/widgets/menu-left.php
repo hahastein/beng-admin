6b179a53ed89a94afb2864abb66fc288ece9e259
hahastein <146119@qq.com>
2019-03-09 23:21:43 +0800

    更新模板功能1

--- views/beng/widgets/menu-left.php
+++ views/beng/widgets/menu-left.php
@@ -26,9 +26,7 @@ use bengbeng\admin\components\handles\TemplateHandle;
         <?php foreach ($menus as $menu): ?>
             <li>
                 <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                    <i class="<?=empty($menu['menu_icon'])?"icon-speedometer":$menu['menu_icon']?>"></i>
-                    <span class="hide-menu">
-                        <?=$menu['menu_name']?>
+                    <i class="<?=empty($menu['menu_icon'])?"icon-speedometer":$menu['menu_icon']?>"></i><span class="hide-menu"><?=$menu['menu_name']?>
                         <?php if(!empty($menu['badge']) && $menu['badge'] > 0): ?>
                         <span class="badge badge-pill badge-cyan ml-auto"><?=$menu['badge']?></span>
                         <?php endif;?>
