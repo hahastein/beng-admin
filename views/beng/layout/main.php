92bf1111a5ec59c6cabefce23fc768a4370ae7e6
hahastein <146119@qq.com>
2019-03-09 17:54:36 +0800

    更新模板功能1

--- views/beng/layout/main.php
+++ views/beng/layout/main.php
@@ -71,9 +71,9 @@ use yii\helpers\Html;
                 <ul class="navbar-nav my-lg-0">
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle waves-effect waves-dark" href="<?=\yii\helpers\Url::to(['/cms'])?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="ti-notepad"></i> <small>内容管理</small>
-
+                        <a class="nav-link waves-effect waves-dark" href="<?=\yii\helpers\Url::to(['/cms'])?>" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="ti-notepad"></i>
+                            <small>内容管理</small>
                         </a>
                     </li>
 
