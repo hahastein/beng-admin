286709e1376328987f0bad6826de2ad34dffc1ba
hahastein <146119@qq.com>
2019-03-09 23:27:01 +0800

    更新模板功能1

--- views/beng/widgets/menu-left.php
+++ views/beng/widgets/menu-left.php
@@ -2,7 +2,6 @@
 use yii\helpers\Url;
 use bengbeng\admin\components\handles\TemplateHandle;
 ?>
-
 <nav class="sidebar-nav">
     <ul id="sidebarnav">
 
@@ -24,23 +23,11 @@ use bengbeng\admin\components\handles\TemplateHandle;
         </li>
         <?php foreach ($menus as $menu): ?>
             <li>
-                <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                    <i class="<?=empty($menu['menu_icon'])?"icon-speedometer":$menu['menu_icon']?>"></i>
-                    <span class="hide-menu">
-                        <?=$menu['menu_name']?>
-                        <?php if(!empty($menu['badge']) && $menu['badge'] > 0): ?>
-                        <span class="badge badge-pill badge-cyan ml-auto"><?=$menu['badge']?></span>
-                        <?php endif;?>
-                    </span>
-                </a>
+                <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="<?=empty($menu['menu_icon'])?"icon-speedometer":$menu['menu_icon']?>"></i><span class="hide-menu"><?=$menu['menu_name']?><?php if(!empty($menu['badge']) && $menu['badge'] > 0): ?><span class="badge badge-pill badge-cyan ml-auto"><?=$menu['badge']?></span><?php endif;?></span></a>
                 <?php if(!empty($menu['parent'])): ?>
                 <ul aria-expanded="false" class="collapse">
                     <?php foreach ($menu['parent'] as $parent): ?>
-                    <li>
-                        <a href="<?= Url::to([$parent['controller'].'/'.$parent['action']]) ?>">
-                            <?=$parent['menu_name']?>
-                        </a>
-                    </li>
+                    <li><a href="<?= Url::to([$parent['controller'].'/'.$parent['action']]) ?>"><?=$parent['menu_name']?></a></li>
                     <?php endforeach;?>
                 </ul>
                 <?php endif;?>
