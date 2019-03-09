2765bd1c0cbb8010ce1dff0465240bcd06f5063a
hahastein <146119@qq.com>
2019-03-10 01:43:37 +0800

    更新模板功能1

--- views/beng/menu/all.php
+++ views/beng/menu/all.php
@@ -1,12 +1,12 @@
 <div class="row page-titles">
     <div class="col-md-5 align-self-center">
-        <h4 class="text-themecolor">Faq</h4>
+        <h4 class="text-themecolor">菜单管理</h4>
     </div>
     <div class="col-md-7 align-self-center text-right">
         <div class="d-flex justify-content-end align-items-center">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
-                <li class="breadcrumb-item active">Faq</li>
+                <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['/'])?>">首页</a></li>
+                <li class="breadcrumb-item active">菜单管理</li>
             </ol>
             <button type="button" class="btn btn-info d-none d-lg-block m-l-15"><i class="fa fa-plus-circle"></i> Create New</button>
         </div>
