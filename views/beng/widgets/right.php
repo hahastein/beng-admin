3574d50b7cedb7691cad291ca08b9fafd6277531
hahastein <146119@qq.com>
2019-03-09 15:47:43 +0800

    更新模板功能1

--- views/beng/widgets/right.php
+++ views/beng/widgets/right.php
@@ -1,3 +1,8 @@
+<li class="nav-item right-side-toggle">
+    <a class="nav-link  waves-effect waves-light" href="javascript:void(0)">
+        <i class="ti-settings"></i>
+    </a>
+</li>
 <!-- .right-sidebar -->
 <div class="right-sidebar">
     <div class="slimscrollright">
@@ -11,7 +16,7 @@
                 <li><a href="javascript:void(0)" data-skin="skin-blue" class="blue-theme">4</a></li>
                 <li><a href="javascript:void(0)" data-skin="skin-purple" class="purple-theme">5</a></li>
                 <li><a href="javascript:void(0)" data-skin="skin-megna" class="megna-theme">6</a></li>
-                <li class="d-block"><b>With Dark sidebar</b></li>
+                <li class="d-block m-t-30"><b>With Dark sidebar</b></li>
                 <li><a href="javascript:void(0)" data-skin="skin-default-dark" class="default-dark-theme ">7</a></li>
                 <li><a href="javascript:void(0)" data-skin="skin-green-dark" class="green-dark-theme">8</a></li>
                 <li><a href="javascript:void(0)" data-skin="skin-red-dark" class="red-dark-theme">9</a></li>
@@ -24,7 +29,27 @@
                 <li>
                     <a href="javascript:void(0)"><img src="../assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>Varun Dhavan <small class="text-success">online</small></span></a>
                 </li>
-
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/2.jpg" alt="user-img" class="img-circle"> <span>Genelia Deshmukh <small class="text-warning">Away</small></span></a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/3.jpg" alt="user-img" class="img-circle"> <span>Ritesh Deshmukh <small class="text-danger">Busy</small></span></a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/4.jpg" alt="user-img" class="img-circle"> <span>Arijit Sinh <small class="text-muted">Offline</small></span></a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/5.jpg" alt="user-img" class="img-circle"> <span>Govinda Star <small class="text-success">online</small></span></a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/6.jpg" alt="user-img" class="img-circle"> <span>John Abraham<small class="text-success">online</small></span></a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/7.jpg" alt="user-img" class="img-circle"> <span>Hritik Roshan<small class="text-success">online</small></span></a>
+                </li>
+                <li>
+                    <a href="javascript:void(0)"><img src="../assets/images/users/8.jpg" alt="user-img" class="img-circle"> <span>Pwandeep rajan <small class="text-success">online</small></span></a>
+                </li>
             </ul>
         </div>
     </div>
