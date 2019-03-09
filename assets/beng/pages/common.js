1d88a31b9ea09fa3741d01e05c784722a354560b
hahastein <146119@qq.com>
2019-03-09 14:27:55 +0800

    更新模板功能1

--- assets/beng/pages/common.js
+++ assets/beng/pages/common.js
@@ -10,4 +10,33 @@ $(function () {
     });
 
     $('.scroll-sidebar, .right-side-panel, .message-center, .right-sidebar').perfectScrollbar();
+
+    /**
+     * Replaces the old skin with the new skin
+     * @param String cls the new skin class
+     * @returns Boolean false to prevent link's default action
+     */
+    function changeSkin(cls) {
+        $.each(mySkins, function (i) {
+            $('body').removeClass(mySkins[i])
+        })
+        $('body').addClass(cls)
+        store('skin', cls)
+        return false
+    }
+
+    function setup() {
+
+        // Add the change skin listener
+        $('[data-skin]').on('click', function (e) {
+            if ($(this).hasClass('knob')) return
+            e.preventDefault()
+            changeSkin($(this).data('skin'))
+        })
+    }
+    setup()
+    $("#themecolors").on("click", "a", function () {
+        $("#themecolors li a").removeClass("working"),
+            $(this).addClass("working")
+    })
 });
--- views/beng/widgets/right.php
+++ views/beng/widgets/right.php
@@ -6,10 +6,10 @@
 <!-- .right-sidebar -->
 <div class="right-sidebar">
     <div class="slimscrollright">
-        <div class="rpanel-title"> Service Panel <span><i class="ti-close right-side-toggle"></i></span> </div>
+        <div class="rpanel-title"> 系统设置 <span><i class="ti-close right-side-toggle"></i></span> </div>
         <div class="r-panel-body">
             <ul id="themecolors" class="m-t-20">
-                <li><b>With Light sidebar</b></li>
+                <li><b>头部样式设定</b></li>
                 <li><a href="javascript:void(0)" data-skin="skin-default" class="default-theme working">1</a></li>
                 <li><a href="javascript:void(0)" data-skin="skin-green" class="green-theme">2</a></li>
                 <li><a href="javascript:void(0)" data-skin="skin-red" class="red-theme">3</a></li>
@@ -26,30 +26,13 @@
             </ul>
             <ul class="m-t-20 chatonline">
                 <li><b>Chat option</b></li>
+
+
                 <li>
                     <a href="javascript:void(0)"><img src="../assets/images/users/1.jpg" alt="user-img" class="img-circle"> <span>Varun Dhavan <small class="text-success">online</small></span></a>
                 </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/2.jpg" alt="user-img" class="img-circle"> <span>Genelia Deshmukh <small class="text-warning">Away</small></span></a>
-                </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/3.jpg" alt="user-img" class="img-circle"> <span>Ritesh Deshmukh <small class="text-danger">Busy</small></span></a>
-                </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/4.jpg" alt="user-img" class="img-circle"> <span>Arijit Sinh <small class="text-muted">Offline</small></span></a>
-                </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/5.jpg" alt="user-img" class="img-circle"> <span>Govinda Star <small class="text-success">online</small></span></a>
-                </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/6.jpg" alt="user-img" class="img-circle"> <span>John Abraham<small class="text-success">online</small></span></a>
-                </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/7.jpg" alt="user-img" class="img-circle"> <span>Hritik Roshan<small class="text-success">online</small></span></a>
-                </li>
-                <li>
-                    <a href="javascript:void(0)"><img src="../assets/images/users/8.jpg" alt="user-img" class="img-circle"> <span>Pwandeep rajan <small class="text-success">online</small></span></a>
-                </li>
+
+
             </ul>
         </div>
     </div>
