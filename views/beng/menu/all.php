84e9dd8bc02d5ce81fc7e81eb69366c2a2ee2d3d
hahastein <146119@qq.com>
2019-03-10 01:54:18 +0800

    更新模板功能1

--- views/beng/menu/all.php
+++ views/beng/menu/all.php
@@ -8,7 +8,34 @@
                 <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['/'])?>">首页</a></li>
                 <li class="breadcrumb-item active">菜单管理</li>
             </ol>
-            <button type="button" class="btn btn-info d-none d-lg-block m-l-15"><i class="fa fa-plus-circle"></i> Create New</button>
+            <button type="button" class="btn btn-info d-none d-lg-block m-l-15"><i class="fa fa-plus-circle"></i> 创建菜单</button>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body p-b-0">
+                <h4 class="card-title">Customtab Tab</h4>
+                <h6 class="card-subtitle">Use default tab with class <code>customtab</code></h6> </div>
+            <!-- Nav tabs -->
+            <ul class="nav nav-tabs customtab" role="tablist">
+                <li class="nav-item"> <a class="nav-link active" data-toggle="tab" href="#home2" role="tab"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Home</span></a> </li>
+                <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#profile2" role="tab"><span class="hidden-sm-up"><i class="ti-user"></i></span> <span class="hidden-xs-down">Profile</span></a> </li>
+                <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#messages2" role="tab"><span class="hidden-sm-up"><i class="ti-email"></i></span> <span class="hidden-xs-down">Messages</span></a> </li>
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+                <div class="tab-pane active" id="home2" role="tabpanel">
+                    <div class="p-20">
+                        <h3>Best Clean Tab ever</h3>
+                        <h4>you can use it with the small code</h4>
+                        <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a.</p>
+                    </div>
+                </div>
+                <div class="tab-pane  p-20" id="profile2" role="tabpanel">2</div>
+                <div class="tab-pane p-20" id="messages2" role="tabpanel">3</div>
+            </div>
         </div>
     </div>
 </div>
