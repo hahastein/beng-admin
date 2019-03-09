c9772b25f2f5c40a22c0cfe6d2b2df0955288361
hahastein <146119@qq.com>
2019-03-10 03:16:48 +0800

    更新模板功能1

--- views/beng/menu/all.php
+++ views/beng/menu/all.php
@@ -8,7 +8,7 @@
                 <li class="breadcrumb-item"><a href="<?= \yii\helpers\Url::to(['/'])?>">首页</a></li>
                 <li class="breadcrumb-item active">菜单管理</li>
             </ol>
-            <button type="button" class="btn btn-info d-none d-lg-block m-l-15"><i class="fa fa-plus-circle"></i> 创建菜单</button>
+            <button type="button" class="btn btn-info d-none d-lg-block m-l-15" data-toggle="modal" data-target="#add-menu"><i class="fa fa-plus-circle"></i> 创建菜单</button>
         </div>
     </div>
 </div>
@@ -88,4 +88,45 @@
             </div>
         </div>
     </div>
+</div>
+
+<!-- Add Contact Popup Model -->
+<div id="add-menu" class="modal fade in" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h4 class="modal-title" id="myModalLabel">Add New Contact</h4> </div>
+            <div class="modal-body">
+                <from class="form-horizontal form-material">
+                    <div class="form-group">
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Type name"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Email"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Phone"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Designation"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Age"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Date of joining"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <input type="text" class="form-control" placeholder="Salary"> </div>
+                        <div class="col-md-12 m-b-20">
+                            <div class="fileupload btn btn-danger btn-rounded waves-effect waves-light"><span><i class="ion-upload m-r-5"></i>Upload Contact Image</span>
+                                <input type="file" class="upload"> </div>
+                        </div>
+                    </div>
+                </from>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-info waves-effect" data-dismiss="modal">Save</button>
+                <button type="button" class="btn btn-default waves-effect" data-dismiss="modal">Cancel</button>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
 </div>
