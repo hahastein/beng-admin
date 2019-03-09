1fd953da31039f04f43bbac2d86aeaef9599e093
hahastein <146119@qq.com>
2019-03-09 23:24:44 +0800

    更新模板功能1

--- views/beng/widgets/menu-left.php
+++ views/beng/widgets/menu-left.php
@@ -22,11 +22,12 @@ use bengbeng\admin\components\handles\TemplateHandle;
                 <li><a href="index4.html">Modern</a></li>
             </ul>
         </li>
-
         <?php foreach ($menus as $menu): ?>
             <li>
                 <a class="has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false">
-                    <i class="<?=empty($menu['menu_icon'])?"icon-speedometer":$menu['menu_icon']?>"></i><span class="hide-menu"><?=$menu['menu_name']?>
+                    <i class="<?=empty($menu['menu_icon'])?"icon-speedometer":$menu['menu_icon']?>"></i>
+                    <span class="hide-menu">
+                        <?=$menu['menu_name']?>
                         <?php if(!empty($menu['badge']) && $menu['badge'] > 0): ?>
                         <span class="badge badge-pill badge-cyan ml-auto"><?=$menu['badge']?></span>
                         <?php endif;?>
@@ -35,16 +36,16 @@ use bengbeng\admin\components\handles\TemplateHandle;
                 <?php if(!empty($menu['parent'])): ?>
                 <ul aria-expanded="false" class="collapse">
                     <?php foreach ($menu['parent'] as $parent): ?>
-                    <li><a href="<?= Url::to([$parent['controller'].'/'.$parent['action']]) ?>"><?=$parent['menu_name']?> </a></li>
+                    <li>
+                        <a href="<?= Url::to([$parent['controller'].'/'.$parent['action']]) ?>">
+                            <?=$parent['menu_name']?>
+                        </a>
+                    </li>
                     <?php endforeach;?>
                 </ul>
                 <?php endif;?>
             </li>
-
         <?php endforeach;?>
-
-
-
         <!--left menu bottom-->
         <li class="nav-small-cap">--- SUPPORT</li>
         <li>
