d261fa1eb026379e646ab677bb4ed837dbce00f6
hahastein <146119@qq.com>
2019-04-10 01:26:22 +0800

    update menu

--- views/beng/merchant/store.php
+++ views/beng/merchant/store.php
@@ -2,22 +2,10 @@
     'title' => '商户管理系统',
     'menu' => ['商户管理' => ''],
     'rightButtons' => [
-        'btn' => [
+        'a' => [
             'title' => '添加商户',
             'target' => 'add-store',
-            'modalParams' => [
-                'submitType' => \bengbeng\admin\components\enum\AdminEnum::ADMIN_SUBMIT_TYPE_AJAX,
-                'submitUrl' => \bengbeng\framework\components\helpers\UrlHelper::to('merchant/store-save'),
-                'control' => [[
-                    'adminName' => [
-                        'model' => 'store-name',
-                        'label' => '商户名称',
-                        'placeholder' => '请填写商户名称',
-                        'tip' => '请按规则填写商户名称',
-                        'type' => 'input.text',
-                    ]
-                ]]
-            ]
+            'href' => \bengbeng\framework\components\helpers\UrlHelper::to('merchant/add-store')
         ]
     ]
 ]) ?>
