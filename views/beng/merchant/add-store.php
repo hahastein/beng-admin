52d924dd31379f9d7faeba26bc6ba1317a94d622
hahastein <146119@qq.com>
2019-04-11 20:49:09 +0800

    update 20190411

--- views/beng/merchant/add-store.php
+++ views/beng/merchant/add-store.php
@@ -83,7 +83,7 @@
                                             <option value="personal">个人</option>
                                             <option value="company">公司</option>
                                         </select>
-                                        <small>店铺的所属类型，仅支持个人与公司</small>
+                                        <small class="text-primary">店铺的所属类型，仅支持个人与公司</small>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -94,7 +94,7 @@
                                             <option value="1">A1</option>
                                             <option value="2">A2</option>
                                         </select>
-                                        <small>店铺的经营类型，不影响实际售卖类型</small>
+                                        <small class="text-primary">店铺的经营类型，不影响实际售卖类型</small>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
@@ -105,7 +105,7 @@
                                             <option value="bj">北京</option>
                                             <option value="sh">上海</option>
                                         </select>
-                                        <small>店铺所在地，如是企业，请选择企业注册所在地</small>
+                                        <small class="text-primary">店铺所在地，如是企业，请选择企业注册所在地</small>
                                     </div>
                                 </div>
                             </div>
@@ -160,49 +160,49 @@
                                     <div class="form-group">
                                         <label for="contact_name">联系人姓名 :</label>
                                         <input type="text" class="form-control" id="contact_name">
-                                        <small class="form-control-feedback">请填写身份证上的真实姓名</small>
+                                        <small class="text-primary">请填写身份证上的真实姓名</small>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="contact_phone">联系人电话 :</label>
                                         <input type="text" class="form-control" id="contact_phone">
-                                        <small>请填写有效的联系人电话，否则会影响审核结果</small>
+                                        <small class="text-primary">请填写有效的联系人电话，否则会影响审核结果</small>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="form-group">
                                         <label for="contact_phone">邮箱地址 :</label>
                                         <input type="text" class="form-control" id="contact_email">
-                                        <small>请填写有效的邮箱地址，验证所使用</small>
+                                        <small class="text-primary">请填写有效的邮箱地址，验证所使用</small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="sfz-zm">上传身份证-正面 (复印件、手机拍照均可):</label>
                                         <input type="file" id="sfz-zm" class="dropify" />
-                                        <small>请上传清晰有效的身份证正面</small>
+                                        <small class="text-primary">请上传清晰有效的身份证正面</small>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="sfz-fm">上传身份证反面 (复印件、手机拍照均可):</label>
                                         <input type="file" id="sfz-fm" class="dropify" />
-                                        <small>请上传清晰有效的身份证反面</small>
+                                        <small class="text-primary">请上传清晰有效的身份证反面</small>
                                     </div>
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="sfznum">身份证号 :</label>
                                         <input type="text" name="sfznum" id="sfznum" class="form-control" />
-                                        <small>身份证号上传后可自动获取，如果有误，请手动更改</small>
+                                        <small class="text-primary">身份证号上传后可自动获取，如果有误，请手动更改</small>
                                     </div>
                                 </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="address">详细地址 :</label>
                                         <textarea name="address" id="address" rows="2" class="form-control"></textarea>
-                                        <small>个人请填写住址，企业请填写公司所在地址</small>
+                                        <small class="text-primary">个人请填写住址，企业请填写公司所在地址</small>
                                     </div>
                                 </div>
                             </div>
