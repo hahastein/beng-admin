85ab4838c94c11b5a6a3f1923e8099b99021ff12
hahastein <146119@qq.com>
2019-04-11 16:23:19 +0800

    update 20190410

--- views/beng/merchant/add-store.php
+++ views/beng/merchant/add-store.php
@@ -110,14 +110,15 @@
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <label for="storeType">支持功能 :</label>
-                                        <input type="checkbox" class="custom-control-input" id="shop">
-                                        <label class="custom-control-label" for="shop">电商</label>
-                                        <input type="checkbox" class="custom-control-input" id="o2o">
-                                        <label class="custom-control-label" for="o2o">外卖</label>
-                                        <input type="checkbox" class="custom-control-input" id="cms">
-                                        <label class="custom-control-label" for="cms">CMS</label>
-                                        <input type="checkbox" class="custom-control-input" id="finance">
-                                        <label class="custom-control-label" for="finance">财务</label>
+
+                                        <input type="checkbox" class="form-control-input" id="shop">
+                                        <label class="form-control-label" for="shop">电商</label>
+                                        <input type="checkbox" class="form-control-input" id="o2o">
+                                        <label class="form-control-label" for="o2o">外卖</label>
+                                        <input type="checkbox" class="form-control-input" id="cms">
+                                        <label class="form-control-label" for="cms">CMS</label>
+                                        <input type="checkbox" class="form-control-input" id="finance">
+                                        <label class="form-control-label" for="finance">财务</label>
                                     </div>
                                 </div>
                             </div>
