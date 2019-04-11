648389b0347f4e6211ecd30c9221cb29878d1144
hahastein <146119@qq.com>
2019-04-11 16:45:07 +0800

    update 20190410

--- views/beng/merchant/add-store.php
+++ views/beng/merchant/add-store.php
@@ -111,22 +111,22 @@
                                     <div class="form-group">
                                         <h5>支持功能 <span class="text-danger">*</span></h5>
                                         <div class="controls">
-                                            <div class="custom-control custom-checkbox">
+                                            <fieldset>
                                                 <input type="checkbox" name="support-tools" class="custom-control-input" id="shop">
                                                 <label class="custom-control-label" for="shop">电商</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox">
+                                            </fieldset>
+                                            <fieldset>
                                                 <input type="checkbox" name="support-tools" class="custom-control-input" id="o2o">
                                                 <label class="custom-control-label" for="o2o">外卖</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox">
+                                            </fieldset>
+                                            <fieldset>
                                                 <input type="checkbox" name="support-tools" class="custom-control-input" id="cms">
                                                 <label class="custom-control-label" for="cms">CMS</label>
-                                            </div>
-                                            <div class="custom-control custom-checkbox">
+                                            </fieldset>
+                                            <fieldset>
                                                 <input type="checkbox" name="support-tools" class="custom-control-input" id="finance">
                                                 <label class="custom-control-label" for="finance">财务</label>
-                                            </div>
+                                            </fieldset>
                                         </div>
                                     </div>
                                 </div>
