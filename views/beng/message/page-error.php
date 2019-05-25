4db103041f59dc9b9f519bffce612c29796fd5b8
hahastein <146119@qq.com>
2019-05-26 00:03:53 +0800

    update 20190512

--- views/beng/message/page-error.php
+++ views/beng/message/page-error.php
@@ -4,37 +4,19 @@ use yii\helpers\Html;
 ?>
 
 <?=\bengbeng\admin\widgets\tools\BreadcrumbWidget::widget([
-    'title' => '系统错误提示',
+    'title' => '页面错误提示',
     'menu' => ['错误提示' => ''],
 ]) ?>
 
 
 <div class="row">
-    <div class="col-12">
-        <div class="card">
-            <div class="card-body">
+    <div class="col-lg-6 col-md-12">
+        <h4 class="card-title">Alert with content </h4>
+        <h6 class="card-subtitle">Alerts can also contain additional HTML elements </h6>
 
-                <div class="col-lg-12">
-                    <h4 class="card-title">Status Code : <?=$exception->getCode()?> | Error Line : <?=$exception->getLine()?></h4>
-                    <h6 class="card-subtitle">以下为系统分析出的错误信息，并以日志形式存储。</h6>
-                    <div class="jumbotron">
-                        <h1 class="display-3"><?= Html::encode($name) ?></h1>
-                        <p class="lead">
-                            <?= nl2br(Html::encode($exception->getMessage())) ?>
-                        </p>
-                        <hr class="my-4">
-                        <pre><?=$exception->getTraceAsString()?></pre>
-                        <hr class="my-4">
-                        <p class="lead">
-                            <a class="btn btn-primary btn-lg" href="/" role="button">返回首页</a>
-                            <a class="btn btn-primary btn-lg" href="/system/document/main" role="button">查看文档</a>
-                        </p>
-                    </div>
-                </div>
-
-
-
-            </div>
+        <div class="alert alert-warning">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
+            <h3 class="text-warning"><i class="fa fa-exclamation-triangle"></i> Warning</h3> This is an example top alert. You can edit what u wish. Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
         </div>
     </div>
 </div>
