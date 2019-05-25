93b4d51a1524547f009df96ed62304c1b959167c
hahastein <146119@qq.com>
2019-05-26 00:10:04 +0800

    update 20190512

--- views/beng/message/page-error.php
+++ views/beng/message/page-error.php
@@ -15,13 +15,26 @@ use yii\helpers\Html;
             <div class="card-body">
 
                 <div class="col-lg-12 col-md-12">
-                    <h4 class="card-title">Alert with content </h4>
-                    <h6 class="card-subtitle">Alerts can also contain additional HTML elements </h6>
+                    <h4 class="card-title">
+                        Alert with content
+                    </h4>
+                    <h6 class="card-subtitle">
+                        Alerts can also contain additional HTML elements
+                    </h6>
 
                     <div class="alert alert-warning">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span> </button>
-                        <h3 class="text-warning"><i class="fa fa-exclamation-triangle"></i> Warning</h3> This is an example top alert. You can edit what u wish. Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <h3 class="text-warning">
+                            <i class="fa fa-exclamation-triangle"></i> Warning
+                        </h3>
+                        This is an example top alert. You can edit what u wish. Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
                     </div>
+
+                    <a class="btn btn-primary btn-lg" href="/" role="button">返回首页</a>
+
+                    <a class="btn btn-primary btn-lg" href="/system/document/main" role="button">查看文档</a>
                 </div>
 
 
