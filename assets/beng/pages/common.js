fa4df99f5dbf2f43349d6351acaf6c8b9acc2eb7
hahastein <146119@qq.com>
2019-05-26 15:15:40 +0800

    update 20190512

--- assets/beng/pages/common.js
+++ assets/beng/pages/common.js
@@ -66,53 +66,20 @@ $(function () {
         "skin-blue-dark",
         "skin-purple-dark",
         "skin-megna-dark"
-    ]
+    ];
 
-    setupListener()
+    loadControl();
+    setupListener();
 
-    /**
-     * Replaces the old skin with the new skin
-     * @param String cls the new skin class
-     * @returns Boolean false to prevent link's default action
-     */
     function changeSkin(cls) {
         $.each(mySkins, function (i) {
             $('body').removeClass(mySkins[i])
-        })
-        $('body').addClass(cls)
-        // store('skin', cls)
+        });
+        $('body').addClass(cls);
         return false
     }
-
-    function setupListener() {
-        jQuery(document).on('click', '.mega-dropdown', function (e) {
-            e.stopPropagation()
-        });
-
-        // Add the change skin listener
-        $('[data-skin]').on('click', function (e) {
-            if ($(this).hasClass('knob')) return
-            e.preventDefault()
-            changeSkin($(this).data('skin'))
-        })
-
-        $("#themecolors").on("click", "a", function () {
-            $("#themecolors li a").removeClass("working"),
-                $(this).addClass("working")
-        })
-
-    }
-
-    $('#tooltipmodals').on('show.bs.modal', function (event) {
-        var modelData = $(event.relatedTarget);
-        var type = modelData.data('type');
-        var content = modelData.parent().find('.hide #r_'+type).html();
-        var modal = $(this);  //get modal itself
-        modal.find('.modal-body').html(content);
-        modal.find('.modal-title').html(type+'资源文件');
-    });
-
-    if($(".tab-wizard")){
+    
+    function loadControl() {
         $(".tab-wizard").steps({
             headerTag: "h6",
             bodyTag: "section",
@@ -128,21 +95,7 @@ $(function () {
 
             }
         });
-    }
 
-    if($("#storeType")){
-        $("#storeType").change(function () {
-            var content = $(this).children('option:selected').val();
-            if(content != ""){
-                $('div[group="storeInfoGroup"][id!='+content+']').hide();
-                $('div[group="storeInfoGroup"][id='+content+']').show();
-            }else{
-                $('div[group="storeInfoGroup"]').hide();
-            }
-        });
-    }
-
-    if($('.dropify')){
         $('.dropify').dropify({
             radius: true,
             messages: {
@@ -151,5 +104,41 @@ $(function () {
         });
     }
 
+    function setupListener() {
+        jQuery(document).on('click', '.mega-dropdown', function (e) {
+            e.stopPropagation()
+        });
 
+        // Add the change skin listener
+        $('[data-skin]').on('click', function (e) {
+            if ($(this).hasClass('knob')) return;
+            e.preventDefault();
+            changeSkin($(this).data('skin'));
+        });
+
+        $("#themecolors").on("click", "a", function () {
+            $("#themecolors li a").removeClass("working");
+            $(this).addClass("working");
+        })
+
+    }
+
+    $('#tooltipmodals').on('show.bs.modal', function (event) {
+        var modelData = $(event.relatedTarget);
+        var type = modelData.data('type');
+        var content = modelData.parent().find('.hide #r_'+type).html();
+        var modal = $(this);  //get modal itself
+        modal.find('.modal-body').html(content);
+        modal.find('.modal-title').html(type+'资源文件');
+    });
+
+    $('storeType').on('change', function () {
+        var content = $(this).children('option:selected').val();
+        if(content != ""){
+            $('div[group="storeInfoGroup"][id!='+content+']').hide();
+            $('div[group="storeInfoGroup"][id='+content+']').show();
+        }else{
+            $('div[group="storeInfoGroup"]').hide();
+        }
+    });
 });
